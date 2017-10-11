329f6c5a268bfdc6b1a8537a84fae4cf74cd2ffc
PizzaCrust <pizzacrustcoder@gmail.com>
2017-10-11 19:17:24 +0700

    get from id

--- src/main/java/online/pizzacrust/roblox/Roblox.java
+++ src/main/java/online/pizzacrust/roblox/Roblox.java
@@ -1,10 +1,15 @@
 package online.pizzacrust.roblox;
 
+import com.google.gson.Gson;
+
+import org.jsoup.Jsoup;
+
 import java.util.Optional;
 
 import online.pizzacrust.roblox.errors.InvalidUserException;
 import online.pizzacrust.roblox.group.Group;
 import online.pizzacrust.roblox.impl.BasicGroup;
+import online.pizzacrust.roblox.impl.BasicProfile;
 import online.pizzacrust.roblox.impl.BasicRobloxian;
 
 public class Roblox {
@@ -20,6 +25,26 @@ public class Roblox {
 
     public static Group get(int groupId) {
         return new BasicGroup(groupId);
+    }
+
+    public static Optional<Robloxian> getUserFromId(int userId) {
+        try {
+            String url = "https://api.roblox.com/Users/" + userId;
+            String response = Jsoup.connect(url).ignoreContentType(true).get().body().text();
+            BasicProfile.Response response1 = new Gson().fromJson(response, BasicProfile.Response
+                    .class);
+            if (response1.Username != null) {
+                return get(response1.Username);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    public static void main(String... args) throws Exception {
+        System.out.println(getUserFromId(1921231347).isPresent());
+        System.out.println(getUserFromId(38043848).get().getUsername());
     }
 
 }
