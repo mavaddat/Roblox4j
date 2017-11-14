0861244934cf2bf9b0b3c29351023ebb26ceed99
PizzaCrust <pizzacrustcoder@gmail.com>
2017-11-14 23:25:04 +0700

    Only make Roblox avaliable in impl and modularize using Java 9

---
+++ RobloxAPI/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module roblox.api {
+
+    exports online.pizzacrust.roblox.api.group;
+    exports online.pizzacrust.roblox.api.errors;
+    exports online.pizzacrust.roblox.api;
+
+    requires java.base;
+
+}
